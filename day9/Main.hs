7069761e4040ba1137ee38a2e9feb9089b03a7be
allmeta <thomasdasd@gmail.com>
2022-12-09 16:25:50 +0100

    day9 part1

--- day9/Main.hs
+++ day9/Main.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Debug.Trace (traceShowId)
 
 main :: IO()
 main = 
@@ -7,24 +8,24 @@ main =
     length .
     nub .
     map snd .
-    scanl solve ((0,0),(0,0)) .
+    foldl' solve [((0,0),(0,0))] .
     map (\[i,n]-> (head i, read n)) .
     map words . 
     lines
   )
 
-solve :: ((Int,Int),(Int,Int)) -> (Char,Int) -> ((Int,Int),(Int,Int))
+solve :: [((Int,Int),(Int,Int))] -> (Char,Int) -> [((Int,Int),(Int,Int))]
 solve p (d,0) = p
-solve p (d,n) = solve np (d,n-1)
+solve (p:pp) (d,n) = solve (np:p:pp) (d,n-1)
   where np = move p $ ds d
 
 move (h@(hx,hy),t@(tx,ty)) (x,y) = (nh,nt)
   where
     nh = (hx+x,hy+y)
-    nt = if mdist nh t > 1 then h else t
+    nt = if mdist nh t then h else t
 
   
-mdist (hx,hy) (tx,ty) = abs (tx-hx) + abs (ty-hy)
+mdist (hx,hy) (tx,ty) = abs (hx-tx) > 1 || abs (hy-ty) > 1
 
 ds 'U' = (0,1)
 ds 'D' = (0,-1)
